fd425435acec73f00885de41460ddec2dda1698a
caizimn <1206886476@qq.com>
2020-07-26 17:26:22 +0800

    Update minComMultiple.c
    long to long long

--- minComMultiple.c
+++ minComMultiple.c
@@ -1,19 +1,18 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-unsigned long minComMultiple(unsigned int a, unsigned int b);
+unsigned long long minComMultiple(unsigned int a, unsigned int b);
 
 void main()
 {
-    printf("gcc ready!\n");
     printf("%u\n",minComMultiple(1,117));
     system("pause");
 }
 
-unsigned long minComMultiple(unsigned int a, unsigned int b)
+unsigned long long minComMultiple(unsigned int a, unsigned int b)
 {
     unsigned int i = 0;
-    unsigned long s = 1;
+    unsigned long long s = 1;
     for (i = 2; i <= a;i++)
     {
         if(a%i==0 && b%i==0)
